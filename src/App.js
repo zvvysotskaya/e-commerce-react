1c5742e75ca2b6a4e9d54543f5a284f6f2c19a61
zvvysotskaya <zhannavyvy@gmail.com>
2019-08-13 23:23:01 -0400

    adding sign form and component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from './pages/homepage/homepage.component';
 import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 
 function App() {
@@ -12,7 +13,8 @@ function App() {
           <Header/>
           <Switch>
               <Route exact path='/' component={HomePage} />
-              <Route path='/shop' component={ShopPage}/>
+              <Route path='/shop' component={ShopPage} />
+              <Route path='/signin' component={SignInAndSignUpPage} />
           </Switch>
           
 
