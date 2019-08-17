d0409319104864a7a10f1471761bc9af9bb95add
zvvysotskaya <zhannavyvy@gmail.com>
2019-08-16 22:57:54 -0400

    implemented firebase utils, included ability to storeauthenticated users into the firestore database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,10 +11,35 @@ const config = {
     messagingSenderId: "280007278479",
     appId: "1:280007278479:web:0d3473da756ab1b7"
 };
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+    //if object exists we query db
+    
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+    if (!snapShot.exists) {
+        const { displayName, email} = userAuth;
+        const createdAt = new Date();
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log("error creating an user", error.massage);
+        }
+    }
+    return userRef;
+
+} 
 firebase.initializeApp(config);
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 //for google authentication
+
+
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './sign-in-and-sign-up.scss';
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 
 
 const SignInAndSignUpPage = () => (
     <div className="sign-in-and-sign-up">
-        <SignIn />        
+        <SignIn /> 
+        <SignUp/>
     </div>
 );
 export default SignInAndSignUpPage;
